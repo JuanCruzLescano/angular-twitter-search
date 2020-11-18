7aa6c001c9a319e2f7754a931f19de1a3ca93ea4
JuanCruzLescano <lesca5172@outlook.com>
2020-11-18 19:03:02 -0300

    fixed route twitter get

--- backend/routes/twitter.js
+++ backend/routes/twitter.js
@@ -1,15 +1,26 @@
 const router = require('express').Router();
 const { twitterClient } = require('../twitterClient');
 
-router.get('/', (req, res) => {
-  const { searchTerms } = req.body;
+router.get('/:searchTerms', (req, res) => {
+  const { searchTerms } = req.params;
   const params = { q: searchTerms, tweet_mode: 'extended', count: 10, lang: 'es', result_type: 'recent' }
 
   if(!searchTerms) res.status(400).send({ text: 'Invalid data' });
 
   twitterClient.get('search/tweets', params) 
     .then(results => {
-      res.status(200).send(results)
+      res.status(200).send(results.data.statuses)
+    })
+    .catch(error => res.send(error))
+})
+
+// Get recent tweets -------- No funciona, es un error por las claves dadas
+router.get('/tweets/recent', (req, res) => {
+  const params = { tweet_mode: 'extended', count: 10}
+
+  twitterClient.get('statuses/home_timeline', params)
+    .then(timeline => {
+      res.status(200).send(timeline.data)
     })
     .catch(error => res.send(error))
 })
